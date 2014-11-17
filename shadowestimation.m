e1f9d61894d057c17e9e6fd9a77069f5312bb41f
Elise Cotton <elise.cotton@gmail.com>
2014-11-16 21:57:49 -0500

    changed typo in displayAppearance.  Loaded images matrix into shadow estimation 

--- shadowestimation.m
+++ shadowestimation.m
@@ -1,6 +1,17 @@
 function [ luminanceperpixel ] = shadowestimation( imagefiles )
+
+%load matrix of image information
+if ~exist('imagematrix.mat')
+    images = matrixGenerate;
+    save('imagematrix.mat', 'images');
+else
+    load('imagematrix.mat', 'images');
+end
+
 %UNTITLED Summary of this function goes here
-%   Detailed explanation goes here
+%   output: f x n matrix of binary values. 
+%       0 -> pixel is in shadow at this frame. 
+%       1 -> pixel is in sunlight at this frame
 b=imread(char(imagesfiles(1)));
 [numr,numc]=size(b);
 luminanceperpixel=zeros(length(imagefiles),numr*numc);
