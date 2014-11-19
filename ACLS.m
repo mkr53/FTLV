95b50b0170f119bad521512b2f2f2b75ef44e499
Elise Cotton <elise.cotton@gmail.com>
2014-11-19 00:31:27 -0500

    continued working on ACLS

--- ACLS.m
+++ ACLS.m
@@ -5,10 +5,6 @@ function [W,H] = ACLS()
     %The concept is to minimize the euclidean error: 
     %||V-WH||^2 = sigma(Vij - (WH)ij)2
     
-    %we will alternate between linear squares solves of W and H:
-    %x = lsqlin(C,d,A,b)solves the linear system C*x = d in the 
-    %least-squares sense, subject to A*x ? b.
-    
     % set up paths to VLFeat functions. 
     % See http://www.vlfeat.org/matlab/matlab.html for VLFeat Matlab documentation
     % This should work on 32 and 64 bit versions of Windows, MacOS, and Linux
@@ -18,9 +14,33 @@ function [W,H] = ACLS()
     %V = images;
     V = randi( [0 256], 100, 500);
     k = 2; %k is between 1 and 5
+    [n,m] = size(V);
     
     [W,H] = initializeACLS(V,k);
-
+    
+    %we will alternate between linear squares solves of W and H:
+    %x = lsqlin(C,d,A,b)solves the linear system C*x = d in the 
+    %least-squares sense, subject to A*x ? b.
+    
+    A = -1 * eye(k);
+    b = zeros(k,1);
+    
+    %for each row of W, solve LCLS: C=H' d = v' x = w'
+    C = double(H');
+    for i = 1:n
+        d = V(i,:)';
+        x = lsqlin(C,d,A,b);        
+        W(i,:) = x';
+    end
+    
+    %for each column of H, solve LCLS: C=W d=v x=h
+    C = double(W);
+    for i = 1:m
+        d = V(:,i);
+        x = lsqlin(C,d,A,b);
+        H(:,i) = x;
+    end
+    
 end
 
 function [W,H] = initializeACLS(V,k)
