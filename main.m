dc804a65cacf665db908610487ff3fde7e63ff91
Elise Cotton <elise.cotton@gmail.com>
2014-12-08 14:53:46 -0500

    introduced new setup for main

--- main.m
+++ main.m
@@ -1,6 +1,4 @@
 function main
-
-%load matrix of image information
 if ~exist('imagematrix.mat')
     fprintf('No image matrix data found. Creating Matrix... \n')
     [images, image_size] = matrixGenerate;
@@ -21,9 +19,24 @@ sky_mask = imresize(imread('skymask.png', 'png'),image_size);
 %of NON-SKY pixels
 F_t = removeSky(images,sky_mask);
 
+[W_sky, H_sky, S_sun, W_sun, H_sun, phi] = FTLV(F_t(:,:,1), sky_mask);
+end
+
+function [W_sky, H_sky, S_sun, W_sun, H_sun, phi] = FTLV(F_t, sky_mask)
+%load matrix of image information
+
+
 %f = # of frames
 %n = # of pixels (non-sky)
 [f,n] = size(F_t);
+
+W_sky = zeros(n,1);
+H_sky = zeros(1,f);
+S_sun = zeros(n,f);
+W_sun = zeros(n,1);
+H_sun = zeros(1,f);
+phi = zeros(n,1);
+
 
 %compute binary shadow estimation for each frame
 %we will do this individualy for the red, green, and blue components
@@ -88,13 +101,13 @@ S = mov_to_matrix(S_mov, sky_mask);
 %S_mov = permute(S_mov, [2 1]);
 
 %S = removeSky(S_mov, sky_mask);
-    A = double(F_t(:,:,1)');
+    A = double(F_t');
 
 if ~exist('skyest.mat')
     fprintf('No sky data found. Creating Matrix... \n')
     %factorize F(t) into Sky: W_sky and H_sky
     fprintf('finding I_sky \n');
-    A = double(F_t(:,:,1)');
+    A = double(F_t');
     [W_sky_r,H_sky_r] = ACLS(A, (1-S)', 'sky');
 
     I_sky = W_sky_r * H_sky_r;
@@ -105,12 +118,15 @@ else
 end
 
 %use this version, to compare decomposition 
-%NewA=A .* (1-S)';
-%[Wcoeff,Hbasis,numIter,tElapsed,finalResidual]=wnmfrule(NewA,1);
+
+NewA=A .* (1-S)';
+fprintf('size of input matrix: \n');
+size(NewA)
+[Wcoeff,Hbasis,numIter,tElapsed,finalResidual]=wnmfrule(NewA,1);
 
 
 %next, we factorize F(t) - I(sky) = I(sun) into its W_sun and H_sun parts
-I_sun = max(double(F_t(:,:,1)) - double(I_sky'), 0);
+I_sun = max(double(F_t) - double(I_sky'), 0);
 fprintf('finding I_sun \n');
 %A = double(F_t(:,:,1)');
 %[W_sun_r, H_sun_r, phi] = ACLS(I_sun', S', 'sun');
@@ -131,7 +147,7 @@ frame = backIntoImage(frame_r, sky_mask);
 
 
 %displayAppearance(F_t, S, I_sky', index);
-displayAppearance(F_t, S_r, threshs_r, I_sky', 'intensity');
+%displayAppearance(F_t, S_r, threshs_r, I_sky', 'intensity');
 
 %frame
 figure
